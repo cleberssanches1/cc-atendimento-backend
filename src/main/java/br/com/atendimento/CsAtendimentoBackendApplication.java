dee39dc882415fc0ade8cc20c171ec78f884ab34
cleber santos sanches <cleberssanches1@gmail.com>
2023-04-09 19:21:00 -0300

    Início do projeto de auto atendimento.

---
+++ src/main/java/br/com/atendimento/CsAtendimentoBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.atendimento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsAtendimentoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsAtendimentoBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/atendimento/CsAtendimentoBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.atendimento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsAtendimentoBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
